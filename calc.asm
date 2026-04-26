7ea5be77a4c8b858da47fa54c3f7274dd5e4a574
Andoumeda <107519532+Andoumeda@users.noreply.github.com>
2026-04-26 20:45:47 -0300

    Prueba con 2 números de 2 dígitos

--- calc.asm
+++ calc.asm
@@ -2,7 +2,8 @@
 .stack 100h
 
 .data
-    msgInput db "Ingrese un numero de 2 digitos: $"
+    msgInput db 13, 10, "Ingrese un numero de 2 digitos: $"
+    msgOpe db 13, 10, "Ingrese operacion: $"
     msgResult db 13, 10, "Resultado: $"
 
 .code
@@ -13,29 +14,62 @@ start:
     mov dx, offset msgInput
     call puts
     call get2DigitNum
+    mov bl, al
+    
+    mov dx, offset msgOpe
+    call puts
+    call getc
+    mov bh, al
+    
+    mov dx, offset msgInput
+    call puts
+    call get2DigitNum
     
     mov dx, offset msgResult
     call puts
     call print2DigitNum
     
+    mov dx, offset msgResult
+    call puts
+    mov al, bl
+    call print2DigitNum
+    
     call finish
     
 get2DigitNum:
+    push bx
+    
     call getc
     mov bh, al
+    sub bh, 30h
     
     call getc
     mov bl, al
+    sub bl, 30h
     
+    mov al, 10
+    mul bh
+    add al, bl
+    
+    pop bx
     ret
     
 print2DigitNum:
-    mov dl, bh
+    push ax
+    push dx
+    
+    aam
+    
+    mov dl, ah
+    add dl, 30h
     call putc
     
-    mov dl, bl
+    mov dl, al
+    add dl, 30h
     call putc
     
+    pop dx
+    pop ax
     ret
 
 getc:
@@ -44,13 +78,21 @@ getc:
     ret
 
 putc:
+    push ax
+    
     mov ah, 2
     int 21h
+    
+    pop ax
     ret
 
 puts:
+    push ax
+    
     mov ah, 9
     int 21h
+    
+    pop ax
     ret
 
 finish:
